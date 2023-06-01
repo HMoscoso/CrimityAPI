518b22c5773f672207d4c92b158886403e3bbd2e
HMoscoso <hillary.moscoso3@gmail.com>
2023-06-01 12:56:01 -0500

    Const to new index database

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,2 +1,8 @@
-export * from './database'
-export { queries } from './query'
+const queries = require('./query');
+const awa = require('./database');
+
+
+
+module.exports = awa.getConnection, awa.sql, awa.queries;
+
+module.exports = queries.queries;
