d2cc52742c0bfd2fa20c888ee006ae49c0029fdf
HMoscoso <hillary.moscoso3@gmail.com>
2023-06-01 02:08:16 -0500

    export default

--- src/config.js
+++ src/config.js
@@ -4,10 +4,11 @@ config();
 
 console.log(process.env.PORT)
 
-export default{
+module.exports = { default: {
     port: process.env.PORT || 4000,
     dbUser: process.env.DB_USER || "",
     dbPassword: process.env.DB_PASSWORD || "",
     dbServer: process.env.DB_SERVER || "",
     dbDataBase: process.env.DB_DATABASE || "",
+}
 }
