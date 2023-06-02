63f81da1c9ef727a39ce2d3bf37f540c733c1176
HMoscoso <hillary.moscoso3@gmail.com>
2023-06-02 00:35:38 -0500

    without slash

--- src/queries/index.js
+++ src/queries/index.js
@@ -4,6 +4,6 @@ const awa = require('./database');
 
 
 module.exports = awa.sql, awa.queries;
-module.exports.getConnection = awa.getConnection();
+module.exports.getConnection = awa.getConnection;
 
 module.exports = queries.queries;
