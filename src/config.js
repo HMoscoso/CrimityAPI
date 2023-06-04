8943f4f46e5c3aacde802c843023b216d1504bca
HMoscoso <hillary.moscoso3@gmail.com>
2023-06-04 02:09:09 -0500

    connection settings

--- src/config.js
+++ src/config.js
@@ -4,11 +4,10 @@ config.config();
 
 console.log(process.env.PORT)
 
-module.exports = { default: {
+module.exports = {  
     port: process.env.PORT || 4000,
     dbUser: process.env.DB_USER || "",
     dbPassword: process.env.DB_PASSWORD || "",
     dbServer: process.env.DB_SERVER || "",
     dbDataBase: process.env.DB_DATABASE || "",
-}
-}
+};
