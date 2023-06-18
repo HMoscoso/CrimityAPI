72a63bf27e0049d8f6ba41025a5c5b39a3d1b6d6
HMoscoso <hillary.moscoso3@gmail.com>
2023-06-18 01:18:22 -0500

    port updates

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import app from './app.js'
 
 async function main(){
     await app.listen(4000);
-    console.log('Server on port 4000');
+    console.log('Server on port' + process.env.PORT);
 }
 
 main();
