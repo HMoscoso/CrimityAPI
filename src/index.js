3fe9c202a73dd7cf43021278b20a756fefb1be50
HMoscoso <hillary.moscoso3@gmail.com>
2023-06-18 02:09:11 -0500

    process env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import app from './app.js'
 
 async function main(){
     await app.listen(4000);
-    console.log('Server on port ' + process.env.PORT + DB_USER);
+    console.log('Server on port ' + process.env.PORT + process.env.DB_USER);
 }
 
 main();
